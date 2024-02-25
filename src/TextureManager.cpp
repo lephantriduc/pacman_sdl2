11151619ba80d298cb322fe48a3c4b930f3ed1de
Le Phan Tri Duc <lephantriduc@gmail.com>
2024-02-25 11:51:15 +0700

    For managing texture

---
+++ src/TextureManager.cpp
@@ -0,0 +1,72 @@
+#include "TextureManager.hpp"
+
+TextureManager::TextureManager() {
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+TextureManager::~TextureManager() {
+    free();
+}
+
+bool TextureManager::load(std::string path) {
+    //Get rid of preexisting texture
+    free();
+
+    //The final texture
+    SDL_Texture* newTexture = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if(loadedSurface == NULL){
+        printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+    }
+    else{
+        //Color key image
+        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        if(newTexture == NULL){
+            printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        }
+        else{
+            //Get image dimensions
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
+        }
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+    //Return success
+    mTexture = newTexture;
+    return mTexture != NULL;
+}
+
+void TextureManager::paint(RGB color) {
+    SDL_SetTextureColorMod(mTexture, color.r, color.g, color.b);
+}
+
+void TextureManager::render(short x, short y, unsigned char facing, SDL_Rect* clip) {
+    //Set rendering space and render to screen
+    SDL_Rect renderQuad = {x, y, mWidth, mHeight};
+
+    //Set clip rendering dimensions
+    if(clip != NULL){
+        renderQuad.w = clip->w ;
+        renderQuad.h = clip->h ;
+    }
+    //Render to screen
+    SDL_RenderCopyEx(renderer, mTexture, clip, &renderQuad, facing * 90.0f, NULL, SDL_FLIP_NONE);
+}
+
+
+void TextureManager::free(){
+    if(mTexture != NULL){
+        SDL_DestroyTexture(mTexture);
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
---
+++ src/TextureManager.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include "Ready.hpp"
+#include "Structs.hpp"
+
+class TextureManager {
+public:
+    TextureManager();
+    ~TextureManager();
+
+    bool load(std::string path);
+    void paint(RGB color);
+    void render(short x = 0, short y = 0, unsigned char facing = 0, SDL_Rect* clip = NULL);
+    void free();
+
+private:
+    SDL_Texture* mTexture;
+    unsigned short mWidth;
+    unsigned short mHeight;
+};
