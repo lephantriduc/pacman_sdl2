b4f5351313cf32a45d8fd2bead8b89daf36640db
Le Phan Tri Duc <lephantriduc@gmail.com>
2024-02-25 11:52:40 +0700

    Clear renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
-#include "Ready.hpp"
+#include "Game.hpp"
 
 int main(int argc, const char* argv[]) {
-
     openSDL();
 
-    gameLoop();
+
+    // gameLoop;
+    Game mGame;
+
+    SDL_Event event;
+    bool quit = false;
+
+    while (!quit) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT)
+                quit = true;
+        }
+
+        // Don't forget to clear the renderer
+        SDL_RenderClear(renderer);
+
+        mGame.draw();
+        SDL_RenderPresent(renderer);
+    }
 
     closeSDL();
 }
