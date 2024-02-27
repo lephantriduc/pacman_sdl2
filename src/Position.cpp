a735ed6e53ff2b20ed7aa197915997abba7341bf
Le Phan Tri Duc <lephantriduc@gmail.com>
2024-02-27 20:46:22 +0700

    Add Position class

---
+++ src/Position.cpp
@@ -0,0 +1,36 @@
+#include "Position.hpp"
+
+Position::Position(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
+
+int Position::getX() const {
+    return this->x;
+}
+
+int Position::getY() const {
+    return this->y;
+}
+
+Position Position::getPos() const {
+    const Position position{this->x, this->y};
+    return position;
+}
+
+void Position::changeX(int newX) {
+    this->x = newX;
+}
+
+void Position::changeY(int newY) {
+    this->y = newY;
+}
+
+void Position::changeCoordinates(int newX, int newY) {
+    this->x = newX;
+    this->y = newY;
+}
+
+bool Position::operator==(Position other) const {
+    return this->x == other.getX() && this->y == other.getY();
+}
